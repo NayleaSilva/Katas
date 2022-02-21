06d09e2eb2ead6c6edb22c97c1c48394efa6aa9f
NayleaSilva <sdan7731@gmail.com>
2022-02-20 20:08:57 -0600

    Katas

---
+++ Kata 1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#Mostrar fecha
+
+from datetime import date
+
+print("Today's date is: ", date.today())
+
+
+#Construir un convertidor de unidades
+
+parsec = 11
+#1 parsec en años luz
+lightyears = 3.26156*11
+
+print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
---
+++ Kata 2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 18 15:48:39 2022
+
+@author: Naylea Peñaloza
+"""
+
+#Prob 1
+#aster = asteroide
+
+aster1=49
+if aster1>25:
+    print('Se ha detectado un asteroide\n')
+else:
+    print('No se ha detectado nada\n')
+    
+#Prob 2
+aster2=19
+if aster2>20:
+    print('Se ha detectado un asteroide, se podrá observar desde la tierra como un rayo de luz\n')
+else:
+    print('No se ha detectado nada\n')
